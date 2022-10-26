380d4453e336fbd7034fc7bc8bba32006d68c316
fortuna <avidia38@gmail.com>
2022-10-26 18:31:48 +0900

    first: must be node_modues server setting

---
+++ android/app/src/main/kotlin/com/example/game_tic_tac_mutiplayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_tic_tac_mutiplayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/room_data_provider.dart';
+import 'screens/create_room_screen.dart';
+import 'screens/game_screen.dart';
+import 'screens/join_room_screen.dart';
+import 'screens/main_menu_screen.dart';
+import 'utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => RoomDataProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: bgColor,
+        ),
+        routes: {
+          MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+          JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+          CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+          GameScreen.routeName: (context) => const GameScreen(),
+        },
+        initialRoute: MainMenuScreen.routeName,
+      ),
+    );
+  }
+}
